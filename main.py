fc24331d4e8d97de3d256cb9e14ab94c243e9e62
Andrew <realhoksly@gmail.com>
2021-06-29 21:11:56 +0300

    V.1.0.3

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 import os
 from ImageFunctions import take_gender_and_age
+import requests
 import random
+import string
 
 
 sep = '/' # need update, specified to os
@@ -15,6 +17,19 @@ Russian_female_second_names_hard_file = 'data' + sep + 'Names' + sep + 'Russian'
 
 # Photos directory
 Photo_dir = 'data/Persons'
+
+# url for persons
+_url = 'https://thispersondoesnotexist.com/image'
+
+# For functions usage
+NEW_FILES = []
+
+
+def generate_random_name(n):
+    if n == 1:
+        return random.choice(string.ascii_letters)
+    else:
+        return random.choice(string.ascii_letters) + generate_random_name(n-1)
 
 
 def take_text_data_from_file(filename):
@@ -57,22 +72,49 @@ def take_photos_and_their_names_from_directory(folder):
     return photos_and_names
 
 
-def put_some_photos_in_folder(folder_name, n):
+def put_some_photos_in_folder(folder_name, n, target_age= 0, target_gender= 0):
     """
+    :param target_gender: can take 'Male' or 'Female' or 0
+    :param target_age: can be '0-2', '4-6', '8-12', '15-20', '25-32', '38-43', '48-53', '60-100' or 0
     :param folder_name: target folder
     :param n:quantity of photos
     :return: None
     """
-    pass
+    _headers = {'User-Agent': 'Mozzila/5.0'}
 
+    while n > 0:
 
-def give_names_to_photos_in_folder(folder, diffiicult = 'hard', language = 'russian'):
+        photo = requests.get(_url, headers=_headers).content
+        filename = folder_name + sep + generate_random_name(8)
+
+        file = open(filename, 'wb')
+        file.write(photo)
+
+        NEW_FILES.append(folder_name + sep + filename)
+        file.close()
+        if target_age == 0 and target_gender == 0:
+            n -= 1
+        else:
+            destroy = False
+            gender, age = take_gender_and_age(filename)
+            if target_age != age and target_age != 0:
+                destroy = True
+            if target_gender != gender and target_gender != 0:
+                destroy = True
+            print(age, target_age, age == target_age)
+            if destroy:
+                os.remove(filename)
+            else:
+                n -= 1
+
+def give_names_to_photos_in_folder(folder, difficult='hard', language='russian', mode='all'):
     """
     Uses function recognize_sex to recognize sex, and after that gives a
     random name and second name to a photo.
 
+    :param mode: rename all files or only some of them
     :param language: language of new set
-    :param diffiicult: types of names (casual or rare)
+    :param difficult: types of names (casual or rare)
     :param folder: target folder
     :return: None
     """
@@ -85,14 +127,14 @@ def give_names_to_photos_in_folder(folder, diffiicult = 'hard', language = 'russ
     #name = 'Jhon'
     #second_name = "Johnston"
 
-    if diffiicult == 'hard' and language == 'russian':
+    if difficult == 'hard' and language == 'rus':
         male_names = take_text_data_from_file(Russian_male_names_hard_file)
         male_second_names = take_text_data_from_file(Russian_male_second_names_hard_file)
         female_names = take_text_data_from_file(Russian_female_names_hard_file)
         female_second_names = take_text_data_from_file(Russian_female_second_names_hard_file)
 
-    for photo in os.listdir(folder):
-        gender, age = take_gender_and_age(folder + sep + photo)
+    for photo in os.listdir(folder) if mode == 'all' else NEW_FILES:
+        gender, age = take_gender_and_age(folder + sep + photo) if mode == 'all' else take_gender_and_age(photo)
 
         if gender == 'Male':
             name = random.choice(male_names)
@@ -105,8 +147,11 @@ def give_names_to_photos_in_folder(folder, diffiicult = 'hard', language = 'russ
         os.rename(folder + sep + photo, folder + sep + new_filename + '.jpeg')
 
 
-def generate_new_set(set_name, n):
+def generate_new_set(set_name, n, mode= 'new', difficult = 'hard', language= 'rus'):
     """
+    :param difficult: difficulty of new set
+    :param language: language of set, can be 'rus' or 'eng'
+    :param mode: new set or for addition. Can take values 'new' or 'addition'
     :param set_name -  name of new set, in which will be placed all of data
             n - quantity of new photos in this set
     :return: Nothing
@@ -115,5 +160,24 @@ def generate_new_set(set_name, n):
     then gives them a name (func gives_names_to_photos_in_folder). Then process is finished.
 
     """
+    folder = Photo_dir + sep + set_name
+    try:
+        os.mkdir(folder)
+    except:
+        pass
+
+    put_some_photos_in_folder(folder, n, target_age=0, target_gender=0)
+    give_names_to_photos_in_folder(folder, mode='all' if mode == 'new' else 'addition', language= language,
+                                   difficult=difficult)
 
     return None
+
+
+class Menu:
+    pass
+
+class Settings:
+    pass
+
+class MainWindow:
+    pass

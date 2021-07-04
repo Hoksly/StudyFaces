b0b7a35167c642dc0ad2fd1aed563c553e8c4698
Andrew <realhoksly@gmail.com>
2021-07-04 08:40:04 +0300

    V.1.0.8

--- GUI.py
+++ GUI.py
@@ -118,9 +118,10 @@ class MainWindow:
         self.input_rect = pygame.Rect(350, 200, 140, 32)
         self.current_person = 0
         self.image = self.persons_list[self.current_person].image
+        self.m_mode = 0
+        self.was_answer_right = True
         # debug print
-        # p
-        # rint(os.getcwd() + sep + data_folder + 'ChelseaMarket-Regular.ttf')
+        # print(os.getcwd() + sep + data_folder + 'ChelseaMarket-Regular.ttf')
 
         self.font = pygame.font.SysFont(data_folder + 'ChelseaMarket-Regular.ttf', 32)
 
@@ -134,20 +135,25 @@ class MainWindow:
                 self.user_text = self.user_text[:-1]
 
             elif event.key == pygame.K_RETURN:
+                self.m_mode += 1
                 if self.persons_list[self.current_person].compare_name(self.user_text):
                     self.score += 1
+                    self.was_answer_right = True
 
-                self.user_text = ''
+                self.was_answer_right = False
 
-                if self.current_person < len(self.persons_list) - 1:
-                    self.current_person += 1
-                    self.image = self.persons_list[self.current_person].image
+                self.user_text = self.persons_list[self.current_person].name
 
+                if self.m_mode % 2 == 0:
+                    if self.current_person < len(self.persons_list) - 1:
+                        self.current_person += 1
+                        self.image = self.persons_list[self.current_person].image
+                        self.user_text = ''
+                        self.was_answer_right = True
 
+                        return '', self.score
 
-                    return 'In game', self.score
-
-                return 'Finish', self.score
+                    return 'Finish', self.score
 
             else:
                 self.user_text += event.unicode
@@ -156,7 +162,7 @@ class MainWindow:
         window.blit(self.background, (0, 0))
 
         pygame.draw.rect(window, pygame.Color('lightblue'), self.input_rect)
-        text_surface = self.font.render(self.user_text, True, (255, 255, 255))
+        text_surface = self.font.render(self.user_text, True, (255, 255, 255)if self.was_answer_right else (255, 0, 0))
         window.blit(text_surface, (self.input_rect.x + 5, self.input_rect.y + 5))
 
         self.input_rect.w = max(100, text_surface.get_width() + 10)
@@ -164,16 +170,48 @@ class MainWindow:
         self.persons_list[self.current_person].draw(100, 100)
 
 
+class FinalScreen:
+    def __init__(self, score, all_score, failed, bg_image = data_folder + 'background.png'):
+        """
+
+        :param score: final score, int
+        :param failed: failed answers (names), list
+        """
+        self.score = score
+        self.failed_answers = failed
+        self.background = pygame.image.load(bg_image)
+        self.font = pygame.font.SysFont(data_folder + 'ChelseaMarket-Regular.ttf', 32)
+        self.all_score = all_score
+
+    def draw(self):
+        window.blit(self.background, (0, 0))
+        score_text = self.font.render('Your final score: ' + '{}/{}'.format(self.score, self.all_score), True, (255, 255, 255))
+        window.blit(score_text, (300 - score_text.get_width()/2, 100))
+
+        failed_answers_text = []
+
+        failed_answers_text.append(self.font.render('Failed:', True, (255, 255, 255)))
+        y = 105
+        y += score_text.get_height()
+
+        for answer in self.failed_answers:
+            failed_answers_text.append(self.font.render(answer, True, (255, 0, 0)))
+
+        for text in failed_answers_text:
+            window.blit(text, (300 - text.get_width()/2, y))
+            y += text.get_height()
+
+
 if __name__ == '__main__':
     run = True
-    Window = MainWindow([Person(pygame.transform.scale(pygame.image.load('data/Persons/Test_set/Герман_Болдырев.jpeg'), (200, 200)), 'GG')])
-
+    # Window = MainWindow([Person(pygame.transform.scale(pygame.image.load('data/Persons/Test_set/Герман_Болдырев.jpeg'), (200, 200)), 'GG')])
+    Window = FinalScreen(2, 16, ['gg', 'GG'])
     while run:
         pygame.time.delay(1000 // 30)
         for el in pygame.event.get():
             if el.type == pygame.QUIT:
                 run = False
-            Window.update_input(el)
+            # Window.update_input(el)
 
         Window.draw()
 
